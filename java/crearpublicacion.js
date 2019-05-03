bdd7ecb7db9cc543eb7558281ba5225933e23a05
martinvelezf <47729374+martinvelezf@users.noreply.github.com>
2019-05-03 18:31:46 -0500

    V2

--- java/crearpublicacion.js
+++ java/crearpublicacion.js
@@ -1,38 +1,44 @@
 function writePublication() {
-    var title = document.getElementById("titulo").value;
+        var title = document.getElementById("titulo").value;
         var desc = document.getElementById("Descripcion").value;
         var cat=document.getElementById("categoria").value;
-        var valor=document.getElementById("Valor").value;
+        var valor=parseFloat(document.getElementById("Valor").value);
         var storageRef = firebase.storage().ref();
         var i1 = document.getElementById("Imagen1");
-        var i2 = document.getElementById("Imagen2");
         var fileRef1=storageRef.child(i1.files[0].name);
         var x=fileRef1.put(i1.files[0]);
+        var newPostKey = firebase.database().ref().child("Publications/"+cat).push().key;
         fileRef1.getDownloadURL().then(function(url) {
           var pic1=url;
             firebase.auth().onAuthStateChanged(function(user) {
               if (user) {
-                // User is signed in.
-                var name = user.displayName;
+                
                 var uid= user.uid;
+                agregaraPerfil(uid, cat, newPostKey, title);
+                var write= firebase.database().ref("Publications/" + cat + "/" + newPostKey).set({
+                    userID: uid,
+                    username: name,
+                    title: title,
+                    description: desc,
+                    category: cat,
+                    valor: valor,
+                    pic1: pic1
+                       }).then(alert("Se ha creado publicacion"));
+                
               }
-              var newPostKey="uid+cat+des";
-              var write= firebase.database().ref("Publications/" + cat + "/" + newPostKey).set({
-               userID: uid,
-              username: name,
-              title: title,
-              description: desc,
-              category: cat,
-              valor: valor,
-              pic1: pic1
-                 });
+             //var user= firebase.database().ref("Users").child(uid).update()
+             
+          
             });
-            alert("Se ha creado publicacion");
-        }).catch(function(error){
-            alter(error);
-          }) ;
-    
-     
+        });   
+  }
+  function agregaraPerfil(uid, cat, key, title) {
+    firebase
+      .database()
+      .ref("Usuario/" + uid + "/" + "Publication" + "/" + cat + "/" + key)
+      .set({
+        titulo: title
+      });
   }
   
   function actualizarIDpublicacion(ID) {
--- java/publicacion.js
+++ java/publicacion.js
@@ -1,4 +1,5 @@
 function publication(cat){
+  
     var botones="<div id='contenedor'><div class='row pt-5'>";
     var Descripcion=new Array();
     var Titulo=new Array();
@@ -17,18 +18,14 @@ function publication(cat){
       var contenido=document.getElementById('new');
       // crea un arreglo con secciones que se vera en el documento html
       for (var i=0;i<Titulo.length;i++){
-        botones=botones+
-        "<div class='col-md-2 offset-md-1'; onclick='verpublicacion("+'"'+cat+'"'+","+'"'+key[i]+'"'+")';>"+
-        "<img src="+img[i]+"> </div>"+
-        "<div class='col-md-6'>"+
-        "<h4>"+Titulo[i]+"</h4>"+
-        "<p >"+ Descripcion[i]+"</p> </div> "+
-       "<button onclick='verpublicacion("+'"'+cat+'"'+","+'"'+key[i]+'"'+")'; class='btn btn-primary'>ver más</button></div>";
-        
+        botones=botones+"<div class='col-md-4 p-5'; style='background-color:white;  onclick=verpublicacion('"+key[i]+"')><img src="+img[i]+"height='70px' width='70px'> <h2>"+ Titulo[i]+"</h2> <p class='text-left'>"+ Descripcion[i]+"</p>  ";
+        botones=botones+"<button onclick='verpublicacion("+'"'+cat+'"'+","+'"'+key[i]+'"'+")'; class='btn btn-danger my-2 my-sm-0'>ver más</button></div>"
+  
       }
-      contenido.innerHTML=botones+"</div>";
+      contenido.innerHTML=botones+'</div><input type="button" value="Ir a inicio" onClick="location.href=location.href">';
     });
   }
+test="img/User.png";
     function verpublicacion(cat,key){
      
       var contenido=document.getElementById('new');
@@ -41,33 +38,91 @@ function publication(cat){
       var database = firebase.database().ref("Publications/"+cat).child(key);
       database.once("value", function(snapshot) {
         snapshot.forEach(function(child) {
+
+          //contenido.innerHTML="<div class='row pt-5'>"
           
-          if (child.key=="pic1" || child.key=="pic2"){
-            imagenes=imagenes+"<img  src="+JSON.stringify(child).split('"').join('')+"/>";
+          if (child.key=="pic1"){
+            imagenes=imagenes+"<div class='col-md-2 offset-md-1' align='left'><img  src="+JSON.stringify(child).split('"').join('')+"/></div>";
           }
+
+          
           else if(child.key=="userID"){
             useruid=JSON.stringify(child);
-            text="<div class='container text-center pt-5'><div  style='background-color:white;' >  ";
+            text=" <div  style='background-color:white;' > <div class='row pt-5'> ";
           }
           else if(child.key=="username"){
-           nombre= nombre+"<h4  onclick=f("+ useruid+")> Publicado por "+JSON.stringify(child).split('"').join('')+"</h4>";
+           nombre= nombre+"<p  onclick=f("+ useruid+")> Publicado por "+JSON.stringify(child).split('"').join('')+"</p></div>";
           }
           else if(child.key=="title"){
-            title=title+"<h1>"+JSON.stringify(child).split('"').join('')+"</h1> ";
+            title=title+"<div class='col-md-6'>  <h3>"+JSON.stringify(child).split('"').join('')+"</h3> ";
           }
           else if(child.key=="categoria"){
-            categoria=categoria+"<h3>"+cat+"</h3> ";
+            categoria=categoria+"<h4>"+cat+"</h4> ";
           }
           else if(child.key=="description"){
-            desc=desc+"<h3>"+JSON.stringify(child).split('"').join('')+"</h3> ";
+            desc=desc+"<p>"+JSON.stringify(child).split('"').join('')+"</p> ";
+          }
+          else if(child.key=="valor"){
+          valor="<div class='col-md-2 offset-md-1' >"+"<h4 align='right'>$"+JSON.stringify(child).split('"').join('')+"</h4>"+"</div>";
           }
          });
-        text=text+title+desc+imagenes+nombre;
+        text=text+imagenes+title+desc+nombre+valor;
+
+      //Enviar Id de la publicacion e ID del Usuario
   
-  
-      contenido.innerHTML= text+'</div></div></div><button onclick="aplicar();" class="btn btn-primary">Buscar</button></p>';
-     
+      contenido.innerHTML= text+'</div></div><input type="button" value="Regresar" onClick="history.back()" ></p>';
+
+
+      contenido.innerHTML = contenido.innerHTML+
+      "<center><button onclick=aplicar(" +
+      useruid +
+      "," +
+      '"' +
+      key +
+      '"' +
+      "," +
+      '"' +
+      cat +
+      '"' +
+      "); class='btn btn-primary'>Aplicar</button></center></p>";
+  });
+}
+function aplicar(publicador, key, cat) {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var uid = user.uid;
+    }
+
+    aplicar2(cat, key, uid, publicador);
+
+    firebase
+      .database()
+      .ref(
+        "Usuario/" +
+          publicador +
+          "/Publication/" +
+          cat +
+          "/" +
+          key +
+          "/interesados/" +
+          uid
+      )
+      .set({
+        interestedID: 0
+      });
+  })
+  alert("¡Has aplicado a la oferta de trabajo!");
+  location.href = "main.html";
+}
+
+function aplicar2(cat, key, uid, publicador) {
+  firebase
+    .database()
+    .ref("Usuario/" + uid + "/request/" + cat + "/" + key)
+    .set({
+      interested: 0,
+      from: publicador
     });
-  }
+}
   
   
